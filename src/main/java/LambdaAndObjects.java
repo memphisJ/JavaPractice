789d7f81568ef64762c16c352401dede64a4bd65
Memphis <h.menphisxp@gmail.com>
2018-07-13 17:48:35 -0400

    The Guava Ordering class has been added.

--- src/main/java/LambdaAndObjects.java
+++ src/main/java/LambdaAndObjects.java
@@ -1,25 +1,34 @@
+import com.google.common.collect.Ordering;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LambdaAndObjects {
     public static void main(String...args){
-        List<String> datesString = Arrays.asList("2018-07-01 09:20 PM","2018-07-02 10:25 AM","2018-07-04 10:50 PM");
+        List<String> datesString = Arrays.asList("2018-07-04 10:20 AM","2018-07-02 09:25 PM","2018-07-01 10:50 PM");
         datesString.forEach(i-> System.out.println(i));
 
         // Change the datesString into a List<Date>
-        System.out.println("Print the same values as LocalDate object");
-        List<LocalDateTime> dates;
+        System.out.println("Print the same values as LocalDate object.");
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a"); // HH -> 24h if hh 12h
-        dates = datesString
-                .stream()
-                .map(date -> LocalDateTime.parse(date,formatter))
-                .collect(Collectors.toList());
+        List<LocalDateTime> dates = datesString
+                                    .stream()
+                                    .map(date -> LocalDateTime.parse(date,formatter))
+                                    .collect(Collectors.toList());
 
         dates.forEach(i-> System.out.println(i));
+
+        if (Ordering.natural().reverse().isOrdered(dates)) {
+            System.out.println("The dates are order in descending order.");
+        } else {
+            System.out.println("The dates are not order in descending order.");
+        }
 
         /*
         List<String> dateStrings = Arrays.asList("12.10.2016", "13.10.2016", "14.10.2016");
